6b8743b3576d6648cb6f4f017895666f4573fb79
Student <student@lab-240-016.cs.csubak.edu>
2023-01-24 09:18:52 -0800

    first commit

---
+++ lab1.cpp
@@ -0,0 +1,14 @@
+//Name: Andres Botello
+//lab1 
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = rand() % 10 + 1;
+    cout << a << endl;
+    cout << "Hello World\n";
+
+
+    return 0;
+}
